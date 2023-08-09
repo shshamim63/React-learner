5ba91ab3638cc40c8288eaa7bd780d4a845b9816
shshamim63 <shshamim63@gmail.com>
2023-08-09 21:19:15 +0600

    Added a simple design for the card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const messages = [
+  'Learn React',
+  'Apply for jobs',
+  'Invest your new income'
+];
+
 function App() {
+  const [stepNumber, setStepNumber] = useState(1)
+
+  const handlePrevious = () => {
+    if(stepNumber > 1) setStepNumber(stepNumber - 1)
+  }
+  const handleNext = () => {
+    if(stepNumber < 3) setStepNumber(stepNumber + 1)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="steps">
+      <div className='numbers'>
+        <div className={stepNumber >= 1 ? 'active': 'inactive'}>1</div>
+        <div className={stepNumber >= 2 ? 'active': 'inactive'}>2</div>
+        <div className={stepNumber >= 3 ? 'active': 'inactive'}>3</div>
+      </div>
+      <p className='message'>Step {stepNumber}: {messages[stepNumber-1]}</p>
+      <div className='buttons'>
+        <button className='previous' onClick={handlePrevious}>Previous</button>
+        <button className='next' onClick={handleNext}>Next</button>
+      </div>
     </div>
   );
 }
