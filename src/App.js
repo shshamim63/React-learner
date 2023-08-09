99b9651a5bc808a8c4906c19711cb9957602f096
shshamim63 <shshamim63@gmail.com>
2023-08-09 21:42:23 +0600

    Introduced appearance section

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ const messages = [
 
 function App() {
   const [stepNumber, setStepNumber] = useState(1)
+  const [isOpen, setIsOpen] = useState(true)
 
   const handlePrevious = () => {
     if(stepNumber > 1) setStepNumber(stepNumber - 1)
@@ -17,18 +18,28 @@ function App() {
     if(stepNumber < 3) setStepNumber(stepNumber + 1)
   }
 
+  const handleOnClick = () => {
+    setIsOpen(!isOpen)
+    setStepNumber(1)
+  }
+
   return (
+    
     <div className="steps">
-      <div className='numbers'>
-        <div className={stepNumber >= 1 ? 'active': 'inactive'}>1</div>
-        <div className={stepNumber >= 2 ? 'active': 'inactive'}>2</div>
-        <div className={stepNumber >= 3 ? 'active': 'inactive'}>3</div>
-      </div>
-      <p className='message'>Step {stepNumber}: {messages[stepNumber-1]}</p>
-      <div className='buttons'>
-        <button className='previous' onClick={handlePrevious}>Previous</button>
-        <button className='next' onClick={handleNext}>Next</button>
-      </div>
+      {isOpen && <button className='close' onClick={handleOnClick}>&times;</button>}
+      {!isOpen &&<button className='close' onClick={handleOnClick}>&#43;</button>}
+      {isOpen && <div className='card'>
+        <div className='numbers'>
+          <div className={stepNumber >= 1 ? 'active': 'inactive'}>1</div>
+          <div className={stepNumber >= 2 ? 'active': 'inactive'}>2</div>
+          <div className={stepNumber >= 3 ? 'active': 'inactive'}>3</div>
+        </div>
+        <p className='message'>Step {stepNumber}: {messages[stepNumber-1]}</p>
+        <div className='buttons'>
+          <button className='previous' onClick={handlePrevious}>Previous</button>
+          <button className='next' onClick={handleNext}>Next</button>
+        </div>
+      </div>}
     </div>
   );
 }
