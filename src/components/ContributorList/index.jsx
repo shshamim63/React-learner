7933d0372b34897455ee5df68e8c532f7101f220
shshamim63 <shshamim63@gmail.com>
2023-08-29 19:46:22 +0600

    Added header to the friend list

--- src/components/ContributorList/index.jsx
+++ src/components/ContributorList/index.jsx
@@ -1,4 +1,4 @@
-import { Grid, List, Paper } from "@mui/material";
+import { Grid, List, Paper, Typography } from "@mui/material";
 
 import { customColor } from "../../style";
 
@@ -10,28 +10,50 @@ const ContributorList = ({ contributors, onSelectFriend, selectedFriend }) => {
   };
 
   return (
-    <Grid item minWidth={400} xs={8} sm={8} md={5}>
+    <Grid
+      item
+      minWidth={400}
+      xs={8}
+      sm={8}
+      md={5}
+      sx={{ background: customColor.amber.light }}
+    >
       {contributors.length > 0 && (
-        <Paper
-          variant="outlined"
-          sx={{
-            padding: { lg: 5, md: 5, background: customColor.blue.light },
-          }}
-        >
-          <List sx={{ background: customColor.orange.light }}>
-            {contributors && [
-              ...contributors.map((friend, index) => (
-                <Contributor
-                  key={friend.id}
-                  user={friend}
-                  index={index}
-                  handleOnSelectFriend={handleOnSelectFriend}
-                  selectedFriend={selectedFriend}
-                ></Contributor>
-              )),
-            ]}
-          </List>
-        </Paper>
+        <>
+          <Paper
+            variant="outlined"
+            sx={{
+              padding: { xs: 2, sm: 2, md: 3, lg: 2 },
+              background: customColor.blue.light,
+            }}
+          >
+            <Typography
+              variant="subtitle2"
+              textAlign="center"
+              sx={{
+                background: customColor.orange.light,
+                marginBottom: 1,
+                borderRadius: 1,
+                border: "0.7px dotted",
+              }}
+            >
+              Friend List
+            </Typography>
+            <List sx={{ background: customColor.orange.light }}>
+              {contributors && [
+                ...contributors.map((friend, index) => (
+                  <Contributor
+                    key={friend.id}
+                    user={friend}
+                    index={index}
+                    handleOnSelectFriend={handleOnSelectFriend}
+                    selectedFriend={selectedFriend}
+                  ></Contributor>
+                )),
+              ]}
+            </List>
+          </Paper>
+        </>
       )}
     </Grid>
   );
