44609e8f81c148f291cfa567d0f55b36ffbf1e2e
shshamim63 <shshamim63@gmail.com>
2023-09-12 20:07:34 +0600

    Added focus when enter is pressed

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useRef } from "react";
+
 import { InputBase, IconButton, Stack } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
 
 const SearchBar = ({ query, setQuery }) => {
+  const inputEl = useRef(null);
+
+  useEffect(() => {
+    const callback = (e) => {
+      if (document.activeElement === inputEl.current) return;
+      if (e.code === "Enter") {
+        inputEl.current.focus();
+        setQuery("");
+      }
+    };
+
+    document.addEventListener("keydown", callback);
+
+    return () => document.removeEventListener("keydown", callback);
+  }, [setQuery]);
+
   const handleOnChange = (e) => {
     setQuery(e.target.value);
   };
@@ -13,6 +31,8 @@ const SearchBar = ({ query, setQuery }) => {
         placeholder="Search Movies"
         inputProps={{ "aria-label": "search google maps" }}
         onChange={handleOnChange}
+        inputRef={inputEl}
+        value={query}
       />
       <IconButton type="button" aria-label="search">
         <SearchIcon />
